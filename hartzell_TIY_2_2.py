53d77755a19f3da9320a90954e55a247d5f2de76
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-08-29 16:29:24 -0400

    Add files via upload

---
+++ hartzell_TIY_2_2.py
@@ -0,0 +1,11 @@
+# First variable with message from Goldfinger (1964)
+super_secret_message = "Do you expect me to talk? No Mr.Bond, I expect you to die!"
+
+# This action prints variable
+print(super_secret_message)
+
+# First Variable with the value of the variable changed to output new message
+super_secret_message = "Do you expect me to talk?, No Mr.Bond, I expect you to print hello world one-billion times, hahahahaha! "
+
+# This action prints variable with changed value
+print(super_secret_message)
