dc654fc4ce5680d1a39feb11eb898d2fdd0613ea
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-08-30 11:41:50 -0400

    Add files via upload

---
+++ hartzell_TIY_2_6.py
@@ -0,0 +1,8 @@
+# Variable for famous person which, in this case, is Bishop Fulton Sheen
+famous_person = "Bishop Fulton Sheen"
+
+# His message
+message = f"{famous_person} - A life is not an accident. It is a responsibility"
+
+# Printing the message 
+print(message)
