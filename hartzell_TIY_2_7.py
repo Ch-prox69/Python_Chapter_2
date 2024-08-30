097608eb6139fd67f659d12f30a7afe7e70e9fd8
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-08-30 12:02:31 -0400

    Add files via upload

---
+++ hartzell_TIY_2_7.py
@@ -0,0 +1,11 @@
+# Name for our variable
+name = "\tChristopher\tHartzitelli\tis\tMy\tAlternate\tItalian\tIdentity"
+
+# Printing my fake Italian Name with rstrip
+print(name.rstrip())
+
+# Printing my fake Italian Name with lstrip
+print(name.lstrip())
+
+# Printing my fake Italian with strip
+print(name.strip())
