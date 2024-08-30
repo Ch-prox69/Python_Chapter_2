c592b1e7c49f270b9750d85dbfc35ea32e429628
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-08-30 08:07:22 -0400

    Add files via upload

---
+++ Hartzell_TIY_2_4.py
@@ -0,0 +1,11 @@
+# This variable represents a name
+name = "McKinstry"
+
+# Print name in lowercase
+print(name.lower())
+
+# Print name in uppercase
+print(name.upper())
+
+# Print name in titlecase
+print(name.title())
