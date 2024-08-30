c4473f7b29f8a709f046e9ed0a165159aa207119
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-08-29 21:26:59 -0400

    Add files via upload

---
+++ hartzell_TIY_2_3.py
@@ -0,0 +1,7 @@
+
+# Variable = King Arthur from Mounty Python's Quest for the Holy Grail (1975)
+King_Arthur = ("Sir Arthur, we need to find shrubbery so that the Knights who say Ni will let us pass! ")
+
+# Print Function
+print(King_Arthur)
+
