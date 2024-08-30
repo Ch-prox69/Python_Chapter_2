c4d001e31bd6d1dad4d161bf10cd4cd5cf2e0cbf
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-08-30 12:21:37 -0400

    Add files via upload

---
+++ hartzell_TIY_2_8.py
@@ -0,0 +1,8 @@
+# Assigning the Value and variable
+filename = 'python_notes.txt'
+
+# Removing the prefix
+updated_filename = filename.removesuffix('.txt')
+
+# Printing the file name with the updated removeprefix feture
+print(updated_filename)
