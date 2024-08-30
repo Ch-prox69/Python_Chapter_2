b9e06cce17dcebf610ba0c579c0b3917a0ffce81
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-08-30 12:34:16 -0400

    Add files via upload

---
+++ hartzell_TIY_2_9.py
@@ -0,0 +1,15 @@
+# Addition
+addition = 7 + 1
+print("The result of the addition problem 7 + 1 is...\n" f'{addition}')
+
+# Subtraction
+subtraction = 9 - 1
+print("The result of the subtraction problem 9 - 1 is...\n" f'{subtraction}')
+
+# Division
+division = 16 / 2
+print("The result of the division problem 16 / 2 is...\n" f'{division}')
+
+# Multiplication
+multiplication = 2 * 4
+print("The result of the multiplication problem 2 * 4 is...\n" f'{multiplication}')
